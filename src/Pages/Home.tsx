7bfedae43f9db36f26e1904495a89731daf5cbaa
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2021-12-04 20:59:54 +0100

    removed unnecessary imports

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import DefaultHelmet from "@/Components/DefaultHelmet";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Generator from "@/Components/Generator";
 
